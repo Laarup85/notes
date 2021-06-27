f3174ab8017d542a37a99450a89193d41cd14b1b
Laarup <laarup85@gmail.com>
2021-06-27 21:17:25 +0300

    templates

--- script.js
+++ script.js
@@ -1,34 +1,46 @@
-try {
-  fetch("./notes/git")
-    .then((res) => res.text())
-    .then((textstring) => {
-      generateItem(textstring, "datastructures");
-    });
-  fetch("./notes/datastructures")
-    .then((res) => res.text())
-    .then((textstring) => {
-      generateItem(textstring, "git");
-    });
-} catch (err) {
-  console.error(err);
-}
+const noteDocuments = ["git", "data-structures"];
+
+noteDocuments.forEach((document) => {
+  try {
+    fetch(`./notes/${document}`)
+      .then((res) => res.text())
+      .then((textstring) => {
+        generateItem(textstring, document);
+      });
+  } catch (err) {
+    console.error(err);
+  }
+});
 
 function generateItem(textstring, id) {
+  let title = id;
+  if (id.includes('-')) title = id.split('-').join(' ');
+
   generateTableOfContentsItem(id);
-  const section = document.createElement('section');
+  const h1 = document.createElement('h1');
+  h1.classList.add('m-section-title');
+  h1.innerHTML = title;
+  const section = document.createElement("section");
+  section.classList.add('m-item-section')
   section.id = id;
-  section.innerHTML = textstring;
-  const main = document.querySelector('main');
+  section.appendChild(h1);
+
+  textstring.split('\n').forEach((item) => {
+    const paragraph = document.createElement('p');
+    paragraph.innerHTML = item;
+    section.appendChild(paragraph);
+  });
+
+  const main = document.querySelector("main");
   main.appendChild(section);
 }
 
 function generateTableOfContentsItem(id) {
   const tableOfContents = document.getElementById("table-of-contents");
-  const li = document.createElement('li');
-  const a = document.createElement('a');
+  const li = document.createElement("li");
+  const a = document.createElement("a");
   a.href = `#${id}`;
   a.innerText = id;
   li.appendChild(a);
   tableOfContents.appendChild(li);
-  console.log(tableOfContents.children);
 }
