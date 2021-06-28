5282632c3c5681603968eb2a1d04463e5b5764d7
mihkel <mihkel@sysdec.ee>
2021-06-28 09:01:03 +0300

    28062021: koodi stiiliparandused

--- script.js
+++ script.js
@@ -1,48 +1,48 @@
 const noteDocuments = ["git", "data-structures", "typescript", "javascript"];
 
 noteDocuments.forEach((document) => {
-  try {
-    fetch(`./notes/${document}`)
-      .then((res) => res.text())
-      .then((textstring) => {
-        generateItem(textstring, document);
-      });
-  } catch (err) {
-    console.error(err);
-  }
+    try {
+        fetch(`./notes/${document}`)
+            .then((res) => res.text())
+            .then((textstring) => {
+                generateItem(textstring, document);
+            });
+    } catch (err) {
+        console.error(err);
+    }
 });
 
 function generateItem(textstring, id) {
-  let title = id;
-  if (id.includes('-')) title = id.split('-').join(' ');
+    let title = id;
+    if (id.includes('-')) title = id.split('-').join(' ');
 
-  generateTableOfContentsItem(id);
-  const h1 = document.createElement('h1');
-  h1.classList.add('m-section-title');
-  h1.innerHTML = title;
-  const section = document.createElement("section");
-  section.classList.add('m-item-section')
-  section.id = id;
-  section.appendChild(h1);
+    generateTableOfContentsItem(id);
+    const h1 = document.createElement('h1');
+    h1.classList.add('m-section-title');
+    h1.innerHTML = title;
+    const section = document.createElement("section");
+    section.classList.add('m-item-section')
+    section.id = id;
+    section.appendChild(h1);
 
-  const ul = document.createElement('ul');
-  textstring.split('\n').forEach((item) => {
-    const li = document.createElement('li');
-    li.innerHTML = item;
-    ul.appendChild(li);
-  });
-  section.appendChild(ul);
+    const ul = document.createElement('ul');
+    textstring.split('\n').forEach((item) => {
+        const li = document.createElement('li');
+        li.innerHTML = item;
+        ul.appendChild(li);
+    });
+    section.appendChild(ul);
 
-  const main = document.querySelector("main");
-  main.appendChild(section);
+    const main = document.querySelector("main");
+    main.appendChild(section);
 }
 
 function generateTableOfContentsItem(id) {
-  const tableOfContents = document.getElementById("table-of-contents");
-  const li = document.createElement("li");
-  const a = document.createElement("a");
-  a.href = `#${id}`;
-  a.innerText = id;
-  li.appendChild(a);
-  tableOfContents.appendChild(li);
+    const tableOfContents = document.getElementById("table-of-contents");
+    const li = document.createElement("li");
+    const a = document.createElement("a");
+    a.href = `#${id}`;
+    a.innerText = id;
+    li.appendChild(a);
+    tableOfContents.appendChild(li);
 }
